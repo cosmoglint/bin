5b5be8e52053d159c0c990667f3cfd46725a5ba2
thedoodler <ramprakashsmail@gmail.com>
2020-11-06 21:27:09 +0530

    square bin

--- javascript_bin.js
+++ javascript_bin.js
@@ -1,1 +1,30 @@
 # javascript nonsense, that I created during testing
+
+
+
+  // if (this.size+this.grow_rate <= min_limit){
+  //   this.grow_rate = this.grow_rate;
+  // }
+  // else if (this.size+this.grow_rate >= max_limit){
+  //   this.grow_rate = -this.grow_rate;
+  // }
+  // this.size = this.size + this.grow_rate;
+  //
+
+// if (this.size == min_limit){
+//   this.grow_rate = 1;
+// }
+// else if (this.size == max_limit){
+//   this.grow_rate = -1;
+// }
+// else if (this.size < min_limit){
+//   this.grow_rate = 1;
+//   this.size = this.size + this.grow_rate;
+// }
+// else if (this.size > max_limit){
+//   this.grow_rate = -1;
+//   this.size = this.size + this.grow_rate;
+// }
+// else{
+//   this.size = this.size + this.grow_rate;
+// }
