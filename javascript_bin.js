0f53cff6a882a3b8488a897a1b8d374d83ed8cc1
thedoodler <ramprakashsmail@gmail.com>
2020-11-08 10:07:51 +0530

    bin fill

--- javascript_bin.js
+++ javascript_bin.js
@@ -33,16 +33,16 @@
 // // genval = (inv) ? map(arr.length*2 - i*j -1,0,arr.length,min_limit,max_limit,true) : map(j*i,0,arr.length,min_limit,max_limit,true);
 // cur_dis = dist(loc_mid.x,loc_mid.y,i,j);
 // genval = map(cur_dis,0.0,corner_dist,min_limit,max_limit);
-// 
+//
 // // cursize = (genval  > max_size) ? (max_size - (genval )%max_size) : genval ;
-// 
+//
 // // cursize = (genval %(max_size*2) > max_size ) ? (max_size*2 - genval %(max_size*2)) : (genval %(max_size*2));
-// 
-// 
+//
+//
 // // inval = (genval  > max_size) ? (-1) : (1);
 // // inval = (genval  > max_size && Math.floor(genval /max_size)%2 == 0) ? (-1*grow_rate) : (1*grow_rate);
 // inval = grow_rate;
-// 
+//
 // if (the_shape=="rectangle"){
 //   arr[i][j] = new rectMover(in_size + (i)*space_dist,in_size + (j)*space_dist,genval,inval,col);
 // }
@@ -50,3 +50,7 @@
 //   arr[i][j] = new circleMover(in_size + (i)*space_dist,in_size + (j)*space_dist,genval,inval,col);
 // }
 
+
+// this.show = function(){
+//     circle(this.x_pos,this.y_pos,10);
+//   }
