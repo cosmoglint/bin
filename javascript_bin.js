4613379832c9b6f9363b458351f437b2f4d5bebc
cosmoglint <ramprakashsmail@gmail.com>
2020-12-29 20:01:42 +0530

    ADD:  insame chrome extension bug

--- javascript_bin.js
+++ javascript_bin.js
@@ -401,3 +401,20 @@
       //       });
       //       // logger();
       //       // alert("this works?");
+
+
+
+      // chrome.runtime.onInstalled.addListener(function() {
+      //   chrome.storage.sync.set({color: '#3aa757'}, function() {
+      //     console.log('The color is green.');
+      //   });
+      //   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      //     chrome.declarativeContent.onpageChanged.addRules([{
+      //       conditions: [new chrome.declarativeContent.PageStateMatcher({
+      //         pageUrl: {hostEquals: 'developer.chrome.com'},
+      //       })
+      //       ],
+      //           actions: [new chrome.declarativeContent.ShowPageAction()]
+      //     }]);
+      //   });
+      // });
