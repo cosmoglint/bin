80fee4b28804aeb595ed2c210873eb90fb3b1c23
ram prakash <ramprakashsmail@gmail.com>
2021-03-11 00:29:49 +0530

    ADD:  rectangle function bin

--- javascript_bin.js
+++ javascript_bin.js
@@ -991,3 +991,22 @@
   // // my_context.fillStyle = 'blue';
   // my_context.rect(50,50,100,100);
   // my_context.fill();
+
+
+
+//  the rectangle class that is used to render the rectangle on the canvas
+
+// function my_rectangle(startx,starty,endx,endy){
+//   this.startx = startx;
+//   this.starty = starty;
+//   this.endx = endx;
+//   this.endy = endy;
+// }
+//
+// my_rectangle.prototype = {
+//   render: function(ctx){
+//     ctx.beginPath();
+//     ctx.rect(this.startx,this.starty,this.endx,this.endy);
+//     ctx.stroke();
+//   }
+// }
