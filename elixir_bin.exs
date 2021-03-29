3fd1c047ca69e9998154562f26aba9a099b14d9e
cosmoglint <ramprakashsmail@gmail.com>
2021-03-29 09:46:24 +0530

    ADD:  elixir bin

---
+++ elixir_bin.exs
@@ -0,0 +1,30 @@
+
+    # defmodule Solution do
+    # #Enter your code here. Read input from STDIN. Print output to STDOUT
+    #     def main(n,_lst) do
+    #
+    #         IO.puts(n + Enum.at([3],0))
+    #     end
+    # end
+    #
+    #
+    # lines = IO.read(:stdio, :all)
+    #
+    #
+    # n = IO.read(:stdio, :line)
+    # lst = IO.read(:stdio, :line)
+    #
+    # n
+    #     |> String.replace("\r","")
+    #     |> String.replace("\n","")
+    #     |> String.to_integer
+    #     |> IO.inspect
+    #
+    #
+    # lst
+    #     |> String.split(" ")
+    #     |> Enum.map(&(String.to_integer(&1)))
+    #     |> IO.inspect
+    #
+    # IO.inspect(n)
+    # Solution.main(n,lst)
