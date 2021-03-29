cc92c3e30174335b8be6125f76c37088d00e11ad
cosmoglint <ramprakashsmail@gmail.com>
2021-03-29 21:54:36 +0530

    ADD:  trash

--- elixir_bin.exs
+++ elixir_bin.exs
@@ -28,3 +28,37 @@
     #
     # IO.inspect(n)
     # Solution.main(n,lst)
+
+
+
+    # def reverse([ val ], ans), do: [ val | ans ]
+    #     def reverse([ head | tail ], ans) do
+    #         [ ans | reverse(tail) ]
+    #     end
+
+
+# defmodule Solution do
+# #Enter your code here. Read input from STDIN. Print output to STDOUT
+#     def main(vals) do
+#
+#     end
+# end
+#
+# vals = String.split(IO.read(:stdio, :all))
+#
+# IO.inspect(vals)
+
+
+
+
+
+
+
+
+#  OK SO MIND BLOWN RIGHT HERE. THIS FUNCTION GETS A LIST AND REVERSES IT BY REDUCING IT. HOW IT DOES IS IT TAKES AN EMPTY LIST AS ACCUMULATOR AND THEN WHEN EACH NEW ELEMENT IS REDUCED WITHT THE FUNCTION IT ADDS IT TO THE TAIL OF THE LIST AND HENCE ALL ELEMENTS ARE REVERSED. THATS AWESOME 
+
+
+#     IO.stream(:stdio, :line)
+# |> Enum.map(&String.trim/1)
+# |> Enum.reduce([], &([&1 | &2]))
+# |> Enum.each(&IO.puts/1)
